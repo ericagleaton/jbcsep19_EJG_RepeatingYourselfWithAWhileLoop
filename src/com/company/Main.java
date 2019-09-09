b1bf78a4bb60af8bccdcd7cf8a88f597749e14a3
ericagleaton <EricaGleaton@comcast.net>
2019-09-09 11:09:13 -0400

    jbcsep19 EJG 20190909 added code secret number is 5. Printout the Guess. Let the player know the guess is too high or too low.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
+//Repeating yourself with a while loop
+
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        Scanner keyboard = new Scanner(System.in);
+        int secretNumber, guess = 0;//guess is initialized to 0
+
+        secretNumber = 5;
+
+        System.out.println("I'm thinking of a number between 1 and 10");
+        System.out.print("Enter the number:");
+        guess = keyboard.nextInt();
+
+        while ( guess != secretNumber )
+        {
+            System.out.println("\n"+ guess + " is not the number.");
+            if(guess>secretNumber) {
+                System.out.println("Your number is too high, choose a number lower.\nTry again.\n");
+            }
+            else {
+                System.out.println("Your number is too low choose a number higher.\nTry again.\n");
+            }
+            System.out.println("Enter the number: ");
+            guess = keyboard.nextInt();
+        }
+
+        System.out.println("You are correct. You win a prize!");
+        keyboard.close();
     }
 }
